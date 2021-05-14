16133c4a0facea3a7f576986c013a6b2e4683f0c
Pau <pvalverdemolina@gmail.com>
2021-05-14 11:49:07 +0900

    set up the router for the pages. Tested rendering a jumbotron component to the home page that is rendered trough the router

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
 import React from "react"
+import {BrowserRouter as Router, Switch, Route, Link} from "react-router-dom"
 import Jumbotron from  "./components/jumbotron"
+import Home from "./pages/Home"
+
 function App() {
+
   return (
-    <Jumbotron />
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/signin">
+          <p>page</p>
+        </Route>
+        <Route path="/signup">
+          <p>page</p>
+        </Route>
+        <Route path="/borwse">
+          <p>page</p>
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
---
+++ src/components/jumbotron/index.js
@@ -0,0 +1,7 @@
+import React from "react"
+function Jumbotron ({}) {
+    return(
+        <p>Jumbotron component.</p>
+    )
+}
+export default Jumbotron
