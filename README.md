0fd7b833a1eb52ec084ba2e30059c7aa7d42b12e
Pau <pvalverdemolina@gmail.com>
2021-05-15 09:57:28 +0900

    Added more styles to jumbotron component

--- README.md
+++ README.md
@@ -2,7 +2,7 @@ A netlfix clone build usign React and Firebase.
 
 Final project for a front end course. 
 
-This project showcases:
+This project showcases the use of:
 - React Router
 - Styled Componets
 - Firebase 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,6 +2,10 @@ import React from "react"
 import Jumbotron from "../components/jumbotron"
 export default function Home() {
     return(
-        <Jumbotron.Container>Jumbotron here</Jumbotron.Container>
+        <Jumbotron.Container>
+            Jumbotron container here
+            <Jumbotron.Title>Title</Jumbotron.Title>
+        <Jumbotron.SubTitle>SubTitle</Jumbotron.SubTitle>
+        </Jumbotron.Container>
     )
 }
