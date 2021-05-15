3d83e5b7c61c00011a270f54c5b8344d46279e01
Pau <pvalverdemolina@gmail.com>
2021-05-16 08:55:14 +0900

    added global styles with styled components

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './app';
+import {GlobalStyles} from "./global-styles"
 
 
 ReactDOM.render(
-  
+  <>
+    <GlobalStyles />
     <App />
+  </>
+
   ,
   document.getElementById('root')
 );
