59fe1f9141769ec806c480242e81b194b811d49e
Pau <pvalverdemolina@gmail.com>
2021-05-17 12:07:13 +0900

    Added Accordion component, styles and container

---
+++ src/components/accordion/styles/accordion.js
@@ -0,0 +1,21 @@
+import styled from "styled-components"
+
+export const Container = styled.div`
+    
+` 
+
+export const Inner = styled.div`
+    
+` 
+
+export const Title = styled.h1`
+
+` 
+
+export const Header = styled.p`
+    
+` 
+
+export const Body = styled.p`
+    
+` 
