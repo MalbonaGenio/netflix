ab4d2fa5f6419a0303a4dc56ae3da7347a8911c2
MalbonaGenio <MalbonaGenio@users.noreply.github.com>
2021-08-03 10:57:33 +0200

    more styling for the select profiles

--- src/components/profiles/styles/profiles.js
+++ src/components/profiles/styles/profiles.js
@@ -1,8 +1,68 @@
 import styled from "styled-components/macro"
 
-export const Container = styled.div``;
-export const Title = styled.h1``;
-export const List = styled.ul``;
-export const Item = styled.li``;
-export const Picture = styled.img``;
-export const Name = styled.p``;
+export const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    max-width: 80%;
+    margin: auto;
+`;
+export const Title = styled.h1`
+    color: #fff;
+    font-weight: 500;
+    font-size: 48px;
+    text-align: center;
+    width: 100%;
+
+`;
+export const List = styled.ul`
+    /* remove default ul styles */
+    padding: 0;
+    marging: 0;
+    display: flex;
+    flex-direction: row;    
+`;
+
+export const Picture = styled.img`
+    width: 100%;
+    max-width: 150px;
+    heigth: auto;
+    cursor: pointer;
+    border: 3px solid black;
+`;
+
+export const Name = styled.p`
+    color: #808080;
+    font-size: 16px;
+    text-overflow: ellipsis;
+
+    &:hover {
+        font-weigth: bold;
+        color: #e5e5e5
+    }
+`;
+
+export const Item = styled.li`
+    max-width: 200px;
+    max-height: 200px;
+    list-style-type: none;
+    text-align: center;
+    margin-rigth: 30px;
+
+    &:hover > ${Picture} {
+        border: 3px solid white;
+    }
+
+    &:hover ${Name} {
+        font-weight: bold;
+        color: white;
+    }
+
+    &:last-of-type {
+        margin-right: 0;
+    }
+`;
+
+
+
